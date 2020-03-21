6c05522c26d95937363927e76feb7473029b3afa
Amirbek2506 <62469848+Amirbek2506@users.noreply.github.com>
2020-03-21 19:42:51 +0300

    created console .net core project

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+
+using System;
 
 namespace MyprojecsApp
 {
@@ -6,16 +8,45 @@ namespace MyprojecsApp
     {
         static void Main(string[] args)
         {
-            int N,S=0;
-            Console.Write("N= ");
+           /*                    //Задача 1;
+            double a,b;
+            a=16.80;
+            b=12.40;
+            Console.WriteLine(Math.Sqrt(a*b));
+                                 //Задача 2;
+            double A,B,C,AC,BC;
+            A=1.40;
+            B=-5.50;
+            C=0.60;
+            AC=Math.Abs (A-C);
+            BC=Math.Abs (B-C);
+            Console.WriteLine($"AC = {AC}");
+            Console.WriteLine($"BC = {BC}");
+            Console.WriteLine($"SUMM = {AC+BC}");
+                                //Задача 3;
+            Double x1=-6.20,y1=5.20,x2=2.10,y2=9.80,Rasstoyanie;
+            Rasstoyanie=Math.Sqrt(Math.Pow ((x2-x1),2)+Math.Pow((y2-y1),2));
+            Console.WriteLine($"Rasstoyanie = {Rasstoyanie}");
+                                //Задача 4;
+            int N,a1,a2;
+            Console.Write("N = ");
             N=Convert.ToInt32(Console.ReadLine());
-            for(int i=0;i<=N;i++)
-            {
-                Console.WriteLine(i);
-            S+=i;
-        
-            }
-                     Console.WriteLine("Summ={0}",S);
+            a1=N%10;
+            a2=N/10;
+            Console.WriteLine($"{a1}{a2}");
+                                //Задача 5;
+            int N;
+            Console.Write("N = ");
+            N=Convert.ToInt32(Console.ReadLine());
+            N=N/60;
+            Console.WriteLine(N);
+                                //Задача 6;
+            int K;
+            Console.Write("K = ");
+            K=Convert.ToInt32(Console.ReadLine());
+            K=((K+1-2)%7)+1;
+            Console.WriteLine($"Номер дня недели: {K}"); */     
+
         }
     }
 }
