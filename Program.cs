855fec1e6a1e5b7b76d92d55a19cadb8cb657666
Amirbek2506 <62469848+Amirbek2506@users.noreply.github.com>
2020-03-21 16:39:36 +0300

    created console .net core project

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyprojecsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N,S=0;
+            Console.Write("N= ");
+            N=Convert.ToInt32(Console.ReadLine());
+            for(int i=0;i<=N;i++)
+            {
+                Console.WriteLine(i);
+            S+=i;
+        
+            }
+                     Console.WriteLine("Summ={0}",S);
+        }
+    }
+}
